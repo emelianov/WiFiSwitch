13b1cb14b98d847b2a9853a9764e837f929a6472
Alexander Emelianov <a.m.emelianov@gmail.com>
2017-05-16 22:45:53 +0500

    rtc

--- WiFiSwitch.ino
+++ WiFiSwitch.ino
@@ -2,14 +2,23 @@
 #include <WiFiClient.h>
 #include <time.h>
 #include <Run.h>
+#include <Wire.h>
+#include <RTClib.h>
+
+#define SDA D2
+#define SCL D3
+#define NTP_CHECK_DELAY 300000;
+#define NTP_MAX_COUNT 3
+
+RTC_DS3231 rtc;
+
 //#include <FS.h>
 //#include <TinyXML.h>
 
 //NTP settings
-#define NTP_CHECK_DELAY 300000;
-#define NTP_MAX_COUNT 3
 String timeServer[NTP_MAX_COUNT];         // NTP servers
 int8_t timeZone = 0;
+bool rtcPresent = false;
 
 //Update time from NTP server
 uint32_t initNtp() {
@@ -17,12 +26,20 @@ uint32_t initNtp() {
     configTime(timeZone * 3600, 0, timeServer[0].c_str(), timeServer[1].c_str(), timeServer[2].c_str());
     return NTP_CHECK_DELAY;
   }
+  if (rtcPresent) {
+    rtc.adjust(DataTime(time(NULL)));
+  }
   return RUN_DELETE;
 }
 
+function getTime() {
+  
+}
+
 void setup(void){
-  wdt_enable(0);
   Serial.begin(74880);
+  Wire.begin(SDA,SCL);
+  rtcPresent = rtc.begin();
   WiFi.mode(WIFI_OFF);
   //SPIFFS.begin();
   //xml.init((uint8_t *)buffer, sizeof(buffer), &XML_callback);
@@ -31,6 +48,5 @@ void setup(void){
 }
 void loop(void){
   taskExec();
-  //wdt_reset();
   yield();
 }
