3b47a123384423019c12c4049cbf0ba9c56bcc7e
Alexander Emelianov <a.m.emelianov@gmail.com>
2017-05-18 18:01:18 +0500

    Split NTP to separate .h file

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 * [Run 2017.3](https://github.com/emelianov/Run)
 * [TinyXML](https://github.com/adafruit/TinyXML)
 * [A fork of Jeelab's fantastic RTC library](https://github.com/adafruit/RTClib)
+* [ESP8266 WiFi Connection manager with web captive portal](https://github.com/tzapu/WiFiManager)
--- WiFiSwitch.ino
+++ WiFiSwitch.ino
@@ -1,50 +1,44 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <time.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
 #include <Run.h>
-#include <Wire.h>
-#include <RTClib.h>
-
-#define SDA D2
-#define SCL D3
-#define NTP_CHECK_DELAY 300000;
-#define NTP_MAX_COUNT 3
-
-RTC_DS3231 rtc;
 
 //#include <FS.h>
 //#include <TinyXML.h>
 
-//NTP settings
-String timeServer[NTP_MAX_COUNT];         // NTP servers
-int8_t timeZone = 0;
-bool rtcPresent = false;
-
-//Update time from NTP server
-uint32_t initNtp() {
-  if (time(NULL) == 0) {
-    configTime(timeZone * 3600, 0, timeServer[0].c_str(), timeServer[1].c_str(), timeServer[2].c_str());
-    return NTP_CHECK_DELAY;
-  }
-  if (rtcPresent) {
-    rtc.adjust(DataTime(time(NULL)));
-  }
-  return RUN_DELETE;
+struct events {
+ uint8_t wifiConnected		= 0;
+ uint8_t resetLongPress 	= 0;
+ uint8_t resetShortPress	= 0;
+ uint8_t turnOffAllSockets	= 0;
 }
-
-function getTime() {
-  
+struct status {
+ bool wifiConnected		= false;
+ bool ntpSync			= false;
+ bool rtcPresent		= false;
+ bool rtcValid			= false;
 }
-
-void setup(void){
+void setup(){
   Serial.begin(74880);
-  Wire.begin(SDA,SCL);
-  rtcPresent = rtc.begin();
   WiFi.mode(WIFI_OFF);
   //SPIFFS.begin();
   //xml.init((uint8_t *)buffer, sizeof(buffer), &XML_callback);
   //taskAdd(start);
   //taskAddWithSemaphore(initNtp, &event.wifiReady);
+
+  WiFiManager wifiManager;
+  //wifiManager.resetSettings();
+  wifiManager.setTimeout(180);
+  while(!wifiManager.autoConnect("AutoConnectAP")) {
+    Serial.println("failed to connect and hit timeout");
+    delay(5000);
+  } 
+
+  //if you get here you have connected to the WiFi
+  Serial.println("connected...yeey :)");
+
 }
 void loop(void){
   taskExec();
