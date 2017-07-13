c4e4ca30cc29a4c68545d156fddf7435c12f179d
Alexander Emelianov <a.m.emelianov@gmail.com>
2017-07-13 17:11:43 +0500

    Put frontend libraries and support files localy

---
+++ tools/README.md
@@ -0,0 +1,5 @@
+# WiFiSwitch Tools
+
+Scripts to download JavaScript and related files to store it localy to ESP flash
+
+GNU wget is required
---
+++ tools/font-awesome.min.cmd
@@ -0,0 +1,5 @@
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://netdna.bootstrapcdn.com/font-awesome/4.4.0/css/../fonts/fontawesome-webfont.eot?v=4.4.0
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://netdna.bootstrapcdn.com/font-awesome/4.4.0/css/../fonts/fontawesome-webfont.woff2?v=4.4.0
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://netdna.bootstrapcdn.com/font-awesome/4.4.0/css/../fonts/fontawesome-webfont.woff?v=4.4.0
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://netdna.bootstrapcdn.com/font-awesome/4.4.0/css/../fonts/fontawesome-webfont.ttf?v=4.4.0
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://netdna.bootstrapcdn.com/font-awesome/4.4.0/css/../fonts/fontawesome-webfont.svg?v=4.4.0#fontawesomeregular
