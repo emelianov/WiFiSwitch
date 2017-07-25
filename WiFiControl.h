862ff49267292d91d95563f0bb35d17b410739cf
Alexander Emelianov <a.m.emelianov@gmail.com>
2017-07-26 00:51:09 +0500

    Set Pump imputs to production

--- WiFiControl.h
+++ WiFiControl.h
@@ -1,10 +1,10 @@
 #pragma once
 // For D1 R2 and mini
-//#define PINS D0, D1, D4, D5, D6, D7, RX, TX
+#define PINS D0, D1, D4, D5, D6, D7, RX, TX
 // For NodeMCU
 //#define PINS D0, D1, D4, D5, D6, D7, D9, D10
 // For DEBUG. Leave RX/TX used for Serial
-#define PINS D0, D1, D4, D5, D6, D7, D6, D7
+//#define PINS D0, D1, D4, D5, D6, D7, D6, D7
 
 // Define enumeration type for convinient override manipulations
 enum OverrideMode { SON, SOFF, SNA };
@@ -97,7 +97,7 @@ class Schedule {
 
 //#define DEFAULT_WAVE 30
 #define DEFAULT_WAVE 30
-#define WAVE_SOC1 4
+#define WAVE_SOC1 0
 #define WAVE_SOC2 1
 #define WAVE_SOC3 2
 #define WAVE_SOC4 3
